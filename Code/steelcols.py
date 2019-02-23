f1397df512b95d69336164a35e1ced7ee9790b03
Ryan Dodyk <ryandodyk@gmail.com>
2019-02-23 00:12:34 -0700

    Started cleaning up code, trying to make functions more reusable

--- Code/steelcols.py
+++ Code/steelcols.py
@@ -79,16 +79,16 @@ def prelimSection ( input, shapes ):
 
 # Iterate on prelimSection
 # Need to add cases for angle and channel members
-def ULSSimple ( input, shapes, st, en, skip ):
+def ULS ( input, shapes ):
     Fy = 350
     E = 200000
     G = 77000
     n = 1.34
     potentials = []
     weights = []
-    for i in range (st, en):
-        if i in skip:
-            continue
+    # for i in range (st, en):
+    #     if i in skip:
+    #         continue
 
         column = Member( shapes[i][:], input[4], input[5], input[0] )
         # Double angle case, but don't feel like making it work really
@@ -100,29 +100,7 @@ def ULSSimple ( input, shapes, st, en, skip ):
         #     #Need to calculate Feyz as per code
         #     Feyz = (Fey+Fez)/(2*omega)*(1-math.sqrt(1-((4*Fey*Fez*omega)/(Fey+Fez)**2)))
         #     Fe = min([Fex, Feyz])
-        if input[5] == "L":
-            if column.b/column.d < 1.7:
-                if 0 <= column.length/column.rx and column.length/column.rx <= 80:
-                    klr = 72 + 0.75*column.length/column.rx
-                elif column.length/column.rx > 80:
-                    klr = 32 + 1.25*column.length/column.rx
-                    if klr > 200:
-                        klr = 200
-                else:
-                    continue
-            else:
-                print("Reference CSA S16-14 $13.3.3.4 for additional design, design is just wack")
-                continue
-            Fe = (math.pi**2*E)/(klr)**2
 
-        else:
-            Fex = (math.pi**2*E)/(((input[4]*input[0])/column.rx)**2)
-            Fey = (math.pi**2*E)/(((input[4]*input[0])/column.ry)**2)
-            F = [Fex, Fey]
-            Fe = min(F)
-
-        lamb = math.sqrt(Fy/Fe)
-        column.Cr = (0.9*column.area*Fy)/((1+lamb**(2*n))**(1/n))/1000
         if column.Cr > input[1]:
             potentials.append([i,column.weight])
 
@@ -131,6 +109,28 @@ def ULSSimple ( input, shapes, st, en, skip ):
     index = weights.index(min(weights))
     column = Member( shapes[potentials[index][0]][:], input[4], input[5], input[0] )
     return column, potentials[index][0]
+
+def crRes ( column, input ):
+    if input[5] == "L":
+        if column.b/column.d < 1.7:
+            if 0 <= column.length/column.rx and column.length/column.rx <= 80:
+                klr = 72 + 0.75*column.length/column.rx
+            elif column.length/column.rx > 80:
+                klr = 32 + 1.25*column.length/column.rx
+                if klr > 200:
+                    klr = 200
+        else: # Gotta do something here
+            print("Reference CSA S16-14 $13.3.3.4 for additional design, design is just wack")
+        Fe = (math.pi**2*E)/(klr)**2
+
+    else:
+        Fex = (math.pi**2*E)/(((input[4]*input[0])/column.rx)**2)
+        Fey = (math.pi**2*E)/(((input[4]*input[0])/column.ry)**2)
+        F = [Fex, Fey]
+        Fe = min(F)
+
+    lamb = math.sqrt(Fy/Fe)
+    column.Cr = (0.9*column.area*Fy)/((1+lamb**(2*n))**(1/n))/1000
 
 def SLSSimple ( k, l, column, index, skip ):
     klrs = [k*l/column.rx, k*l/column.ry]
@@ -144,40 +144,6 @@ def SLSSimple ( k, l, column, index, skip ):
         skip.append(index)
         return False, skip
 
-# Designs beam columns, but only with point moments at the top of the column
-def ULSHard ( input, shapes, st, en, skip ):
-    Fy = 350
-    E = 200000
-    G = 77000
-    n = 1.34
-    potentials = []
-    eff = []
-    weights = []
-    for i in range (st, en):
-        if i in skip:
-            continue
-        for i in (1, 3):
-            #Find compressive resistance
-            column, index = ULSSimple ( input, shapes, st, en, skip )
-            #Find moment resistance
-            print("Find moment resistance here")
-
-            #Find column efficiency
-            column.efficiency = input[1]/column.Cr + (0.85*U1x*input[2])/column.Mrx + (B*U1y*input[3])/column.Mry
-            #Test to see if column meets criteria
-            eff.append(column.efficiency)
-        if max(eff) <= .85 and max(eff) > .6:
-            potentials.append([i,column.weight])
-
-    for j in range(0, len(potentials)):
-        weights.append(potentials[j][1])
-    index = weights.index(min(weights))
-    column = Column( shapes[potentials[index][0]][:], input[4], input[5], input[0] )
-    return column, potentials[index][0]
-
-def SLSHard ( column, index, skip ):
-    print ("Hi")
-
 ### Main Body
 shapes = []
 with open('../Assets/aisc-shapes-database-v15.csv', 'r') as steelCSV:
@@ -188,17 +154,9 @@ with open('../Assets/aisc-shapes-database-v15.csv', 'r') as steelCSV:
 
 [l, P, Mx, My, k, section] = colProperties()
 st, en = prelimSection( [l, P, Mx, My, k, section], shapes )
-skip = []
-passed = False
-if Mx == 0 and My == 0:
-
-    while passed == False:
-        column, index = ULSSimple ( [l, P, Mx, My, k, section], shapes, st, en, skip )
-        passed, skip = SLSSimple ( k, l, column, index, skip )
-
-else:
-    while passed == False:
-        column, index = ULSHard ( [l, P, Mx, My, k, section], shapes, st, en, skip )
-        passed, skip = SLSHard ( column, index, skip )
+# Just check all members to see if they passed ULS and SLS cases
+for i in range (st, en):
+    column, index = ULS ( [l, P, Mx, My, k, section], shapes )
+    potentials = SLS ( k, l, column, index )
 
 print(column.name)
