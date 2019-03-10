fd09d8ede69ea774de3d9092b5ba3617267a07dd
Ryan <ryandodyk@gmail.com>
2019-03-10 15:24:30 -0600

    Got the automatic input working!

--- Code/steelcols.py
+++ Code/steelcols.py
@@ -1,5 +1,6 @@
 # Program to design steel beams as per CSA S16
 import math
+import numpy as np
 from memberclasses import Member
 
 ### Functions
@@ -49,9 +50,11 @@ def colProperties ():
     return (l, P, Mx, My, k, section)
 
 # Choose a preliminary section for analysis
-def prelimSection ( shapes ):
-    st = next(i for i in (range(len(shapes))) if shapes[i][0] == desInfo[5])
-    en = next(i for i in reversed(range(len(shapes))) if shapes[i][0] == desInfo[5])-1
+def prelimSection ( shapes, desInfo ):
+    c = np.array(shapes)
+    col = c[:,0] 
+    st = np.amin(np.where(col==desInfo[5])) 
+    en = np.amax(np.where(col==desInfo[5])) 
     if desInfo[5] == "HSS":
         st = 1914
 
@@ -142,6 +145,84 @@ def SLS ( k, l, column, index ):
     
     return klr
 
+def mainloop(desInfo, shapes):
+    potentials = []
+    weights = []
+    st, en = prelimSection( shapes, desInfo )
+    # Just check all members to see if they passed ULS and SLS cases
+    for i in range (st, en):
+        column = Member ( shapes[i][:], desInfo[4], desInfo[5], desInfo[0] )
+
+        # Check ULS Failure
+        column, efficiency = ULS ( desInfo, shapes, column )
+        # Check SLS Failure
+        klr = SLS ( desInfo[4], desInfo[0], column, i )
+
+        # Go through checks to see if member passes
+        if efficiency < 0.5 or efficiency >= 0.9:
+            continue
+        elif column.secClass == 4:
+            continue
+        elif klr > 200:
+            continue
+        else:
+            print(column.name, column.Cr)
+            potentials.append([i, column.weight])
+        
+    for j in range(0, len(potentials)):
+        weights.append(potentials[j][1])
+    index = weights.index(min(weights))
+    column = Member( shapes[potentials[index][0]][:], desInfo[4], desInfo[5], desInfo[0] )
+
+    column, efficiency = ULS(desInfo, shapes, column) 
+    print(column.name, column.Cr)
+
+    try:
+        fileName = desInfo[6]
+        writeFile(fileName, column, klr)
+    except:
+        save = input("Save this column? ")
+        if save.upper() == "Y" or save.upper() == "":
+            fileName = input("Name of this Column: ")
+            writeFile(fileName, column, klr)
+        else:
+            pass
+
+def writeFile(fileName, column, klr):
+        file = "../Output/{}.tex".format(fileName)
+        f = open(file, "w+")
+        # Begin LaTeX!
+        # Outputs a .tex file with a perfectly formatted design
+        output = """ \\documentclass{{article}}\n\n
+                            \\usepackage{{amsmath}}\n\n
+                            \\begin{{document}}
+                            Factored Loads for {2}:\\\\
+                            Axial Compression: {0[1]} kN, Mx: {0[2]} kN$\\cdot$m, My: {0[3]} kN$\\cdot$m\\\\
+                            Length: {1.length} mm, k: {1.k}\\\\
+                            Resulted in the design of a {1.name} class {1.secClass} steel column\\\\
+                            \\textbf{{Resistance Calculation:}}\\\\
+                            \\begin{{align*}}
+                            F_{{e}} =& \\frac{{\\pi^2 E}}{{(\\frac{{kl}}{{r}})^2}} && &\\oint 13.3.1\\\\
+                            F_{{ex}} =& \\frac{{\\pi^2 E}}{{(\\frac{{{1.k} \\cdot {1.length} mm}}{{{1.rx:0.2f} mm}})^2}} && \\\\
+                            F_{{ey}} =& \\frac{{\\pi^2 E}}{{(\\frac{{{1.k} \\cdot {1.length} mm}}{{{1.ry:0.2f} mm}})^2}} && \\\\
+                            F_{{e}} =& min \\begin{{cases}}
+                            F_{{ex}}\\\\
+                            F_{{ey}}\\\\
+                            \\end{{cases}} = {1.Fe:0.2f} MPa && &\\oint 13.3.1\\\\
+                            \\lambda =& \\sqrt{{\\frac{{F_{{y}}}}{{F_{{e}}}}}} && &\\oint 13.3.1\\\\
+                            \\lambda =& \\sqrt{{\\frac{{350 MPa}}{{{1.Fe:0.2f} MPa}}}} = {1.lamb:0.2f} &&\\\\
+                            C_{{r}} =& \\frac{{\\phi A F_{{y}}}}{{(1+\\lambda^{{2n}})^\\frac{{1}}{{n}}}} && &\\oint 13.3.1\\\\
+                            C_{{r}} =& \\frac{{0.9 \\cdot {1.area} mm \\cdot 350 MPa}}{{(1+{1.lamb:0.2f}^{{2 \\cdot 1.34}})^\\frac{{1}}{{1.34}}}} &&\\\\
+                            C_{{r}} =& {1.Cr:0.0f} kN\\\\
+                            \\text{{SLS Check:}}\\\\
+                            \\frac{{kl}}{{r}} \\leq& 200 && &\\oint 10.4\\\\
+                            =& \\frac{{{1.k}\\cdot {1.length:0.2f}}}{{{1.ry:0.2f}}}&&\\\\
+                            =& {3:0.2f}
+                            \\end{{align*}}
+                            \\end{{document}}""".format(desInfo, column, fileName, klr)
+        f.write(output)
+        f.close()
+
 ### Main Body
 shapes = []
 with open('../Assets/aisc-shapes-database-v15.csv', 'r') as steelCSV:
@@ -150,73 +231,20 @@ with open('../Assets/aisc-shapes-database-v15.csv', 'r') as steelCSV:
         # Metric shape names @ column 82
         # Check excel file for referencing columns
 
-desInfo = colProperties() # [l,P,Mx,My,k,section]
-potentials = []
-weights = []
-st, en = prelimSection( shapes )
-# Just check all members to see if they passed ULS and SLS cases
-for i in range (st, en):
-    column = Member ( shapes[i][:], desInfo[4], desInfo[5], desInfo[0] )
+auto = input("Design Multiple Columns from File? (y) ")
 
-    # Check ULS Failure
-    column, efficiency = ULS ( desInfo, shapes, column )
-    # Check SLS Failure
-    klr = SLS ( desInfo[4], desInfo[0], column, i )
-
-    # Go through checks to see if member passes
-    if efficiency < 0.5 or efficiency >= 0.9:
-        continue
-    elif column.secClass == 4:
-        continue
-    elif klr > 200:
-        continue
-    else:
-        print(column.name, column.Cr)
-        potentials.append([i, column.weight])
-    
-for j in range(0, len(potentials)):
-    weights.append(potentials[j][1])
-index = weights.index(min(weights))
-column = Member( shapes[potentials[index][0]][:], desInfo[4], desInfo[5], desInfo[0] )
-
-column, efficiency = ULS(desInfo, shapes, column) 
-print(column.name)
-
-save = input("Save this column? ")
-if save.upper() == "Y" or save.upper() == "":
-    fileName = input("Name of this Column: ")
-    file = "../Output/{}.tex".format(fileName)
-    f = open(file, "w+")
-    # Begin LaTeX!
-    # Outputs a .tex file with a perfectly formatted design
-    output = """ \\documentclass{{article}}\n\n
-                        \\usepackage{{amsmath}}\n\n
-                        \\begin{{document}}
-                        Factored Loads for {2}:\\\\
-                        Axial Compression: {0[1]} kN, Mx: {0[2]} kN$\\cdot$m, My: {0[3]} kN$\\cdot$m\\\\
-                        Length: {1.length} mm, k: {1.k}\\\\
-                        Resulted in the design of a {1.name} class {1.secClass} steel column\\\\
-                        \\textbf{{Resistance Calculation:}}\\\\
-                        \\begin{{align*}}
-                        F_{{e}} =& \\frac{{\\pi^2 E}}{{(\\frac{{kl}}{{r}})^2}} && &\\oint 13.3.1\\\\
-                        F_{{ex}} =& \\frac{{\\pi^2 E}}{{(\\frac{{{1.k} \\cdot {1.length} mm}}{{{1.rx:0.2f} mm}})^2}} && \\\\
-                        F_{{ey}} =& \\frac{{\\pi^2 E}}{{(\\frac{{{1.k} \\cdot {1.length} mm}}{{{1.ry:0.2f} mm}})^2}} && \\\\
-                        F_{{e}} =& min \\begin{{cases}}
-                        F_{{ex}}\\\\
-                        F_{{ey}}\\\\
-                        \\end{{cases}} = {1.Fe:0.2f} MPa && &\\oint 13.3.1\\\\
-                        \\lambda =& \\sqrt{{\\frac{{F_{{y}}}}{{F_{{e}}}}}} && &\\oint 13.3.1\\\\
-                        \\lambda =& \\sqrt{{\\frac{{350 MPa}}{{{1.Fe:0.2f} MPa}}}} = {1.lamb:0.2f} &&\\\\
-                        C_{{r}} =& \\frac{{\\phi A F_{{y}}}}{{(1+\\lambda^{{2n}})^\\frac{{1}}{{n}}}} && &\\oint 13.3.1\\\\
-                        C_{{r}} =& \\frac{{0.9 \\cdot {1.area} mm \\cdot 350 MPa}}{{(1+{1.lamb:0.2f}^{{2 \\cdot 1.34}})^\\frac{{1}}{{1.34}}}} &&\\\\
-                        C_{{r}} =& {1.Cr:0.0f} kN\\\\
-                        \\text{{SLS Check:}}\\\\
-                        \\frac{{kl}}{{r}} \\leq& 200 && &\\oint 10.4\\\\
-                        =& \\frac{{{1.k}\\cdot {1.length:0.2f}}}{{{1.ry:0.2f}}}&&\\\\
-                        =& {3:0.2f}
-                        \\end{{align*}}
-                        \\end{{document}}""".format(desInfo, column, fileName, klr)
-    f.write(output)
-    f.close()
+if auto.upper() == "Y" or auto.upper() == "":
+    fInput = []
+    desInfo = []
+    autoName = input("Name of file to import from: ")
+    file = "../Assets/{}.csv".format(autoName)
+    with open(file, 'r') as autoSheet:
+        for row in autoSheet:
+            fInput.append(row.strip().split(','))
+        for z in range(1, len(fInput)):
+            desInfo = [float(fInput[z][1]), float(fInput[z][2]), float(fInput[z][3]), float(fInput[z][4]), float(fInput[z][5]), fInput[z][6], fInput[z][0]]
+            mainloop(desInfo, shapes)
 else:
-    pass
+    desInfo = colProperties() # [l,P,Mx,My,k,section]
+    mainloop(desInfo, shapes)
+
